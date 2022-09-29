fb8c9b14adc5abf845304825d182710fc17fd37a
daris82 <106485107+daris82@users.noreply.github.com>
2022-09-29 13:25:06 +0200

    Add files via upload

---
+++ dwes01/comerciales.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Nuestros Comerciales</h1>
+
+    <?php
+        include 'amigo.php';
+
+
+    ?>
+    <ul>
+        <li>John Williams</li>
+        <li>dd</li>
+        <li>dd</li>
+        <li>dd</li>
+    </ul>
+
+    <?php
+        include 'amigo.php';
+
+
+    ?>
+</body>
+</html>
+
+
---
+++ dwes01/etc/conf.php
@@ -0,0 +1,4 @@
+define ('COMERCIAL1','John Williams <johnwilliams@wetwater.com>');
+define ('COMERCIAL2','Pedro Lopez <pedrolopez@wetwater.com>');
+define ('COMERCIAL3','Sara Sanchez <sarasanchez@wetwater.com>');
+define ('COMERCIAL4','Juan Navarro <juannavarro@wetwater.com>');
